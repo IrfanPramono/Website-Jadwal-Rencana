e93eb9678009731b00da1c038c48102b7f774891
Irfan Pramono <mirfanpramono1905@gmail.com>
2025-01-20 05:09:08 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,49 @@
+let todolist = JSON.parse(localStorage.getItem('todolist')) || [];
+
+function todolistDo() {
+    let todolistHTML = '';
+
+    todolist.forEach((value, i) => {
+        const { inputTodo, inputDate } = value;
+        const htmlTodo = `
+        <p>
+            ${inputTodo} (${inputDate})
+            <button onclick="removeTodo(${i})">Hapus</button>
+        </p>`;
+        todolistHTML += htmlTodo;
+    });
+
+    document.querySelector('#js-todolist').innerHTML = todolistHTML;
+}
+
+function addTodo() {
+    const inputElement = document.querySelector('#js-input');
+    const inputTodo = inputElement.value;
+    const dateElement = document.querySelector('#js-dateInput');
+    const inputDate = dateElement.value;
+
+    if (inputTodo && inputDate) {
+        todolist.push({
+            inputTodo,
+            inputDate
+        });
+
+        h
+        localStorage.setItem('todolist', JSON.stringify(todolist));
+
+        inputElement.value = ''; 
+        dateElement.value = ''; 
+        todolistDo();
+    } else {
+        alert('Harap masukkan rencana dan tanggal!');
+    }
+}
+
+function removeTodo(index) {
+    todolist.splice(index, 1); 
+    localStorage.setItem('todolist', JSON.stringify(todolist)); 
+    todolistDo(); 
+}
+
+
+todolistDo();
